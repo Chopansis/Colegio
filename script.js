807006b0bce69c081d8b5ff447dbf4431f1d1aeb
Chopansis <francisgarcianirenstein@gmail.com>
2024-10-28 11:35:08 -0300

    Update 1 script.js

--- script.js
+++ script.js
@@ -2,4 +2,13 @@ function toggleContent(contentId) {
   const content = document.getElementById(contentId);
   content.style.display = content.style.display === 'block' ? 'none' : 'block';
 }
+// Agrandar fotiños
+document.querySelectorAll('.character-image').forEach(img => {
+  img.addEventListener('mouseenter', () => {
+    img.style.transform = 'scale(1.2)';
+  });
+  img.addEventListener('mouseleave', () => {
+    img.style.transform = 'scale(1)';
+  });
+});
 
